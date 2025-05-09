b89df9cc6bb903e6fe36333031fb7bb3b3c649d3
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2025-05-09 11:59:39 +0200

    feat: refactor search API to improve URL construction and enhance code readability

--- api/search.js
+++ api/search.js
@@ -1,8 +1,33 @@
 const dotenv = require("dotenv")
 const path = require("path")
+
+/**
+ * Handles the /api/search endpoint.
+ */
 module.exports = {
+    /**
+     * Searches for mods on Modrinth.
+     * @param {import("express").Request} req - The request object.
+     * @param {import("express").Response} res - The response object.
+     */
     get: async (req, res) => {
-        const response = await fetch("https://api.modrinth.com/v2/search?query=" + req.query.q + "&facets=[[\"project_type:mod\"], [\"versions:1.21.1\"], [\"categories:neoforge\"]]")
+        // Construct the URL for the Modrinth API
+        const url = "https://api.modrinth.com/v2/search"
+        // Add the query parameter from the request
+        const params = new URLSearchParams({
+            query: req.query.q,
+            facets: JSON.stringify([
+                // Only search for mods
+                ["project_type", "mod"],
+                // Only search for mods that support Minecraft 1.21.1
+                ["versions", "1.21.1"],
+                // Only search for mods that are in the neoforge category
+                ["categories", "neoforge"],
+            ]),
+        })
+        // Fetch the data from Modrinth
+        const response = await fetch(`${url}?${params}`)
+        // Return the data as JSON
         res.json(await response.json())
-    }
+    },
 }
